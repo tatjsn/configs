1ac52a1694a5675aa997ab8fd2000476f8f91366
Taz Jackson <tatjsn@users.noreply.github.com>
2026-01-10 11:46:54 +0000

    Add shortcuts and more

--- emacs.el
+++ emacs.el
@@ -1,5 +1,5 @@
 ;; packages
-;; find-file-in-project rainbow-delimiters
+;; vertico find-file-in-project rainbow-delimiters
 
 
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
@@ -7,6 +7,7 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (xterm-mouse-mode 1)
+(electric-pair-mode 1)
 (setq-default indent-tabs-mode nil)
 (setq inhibit-splash-screen t)
 (setq column-number-mode t)
@@ -31,3 +32,10 @@
 ;; rainbow-delimiters
 (require 'rainbow-delimiters)
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+
+;; better completion buffer
+(vertico-mode)
+
+;; shortcuts
+(global-set-key [f1] 'ffip)
+(global-set-key [f12] 'rgrep)
